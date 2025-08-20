52928df46170187334ca7b96fe02163d622b387a
Fresia Pichardo <pfresia22@gmail.com>
2025-08-20 15:22:44 -0600

    actualiza dependencias y conexión MongoDB

--- database/connection.js
+++ database/connection.js
@@ -1,18 +1,15 @@
 const mongoose = require("mongoose");
 
-// URL de conexión a MongoDB
-const dbURI = 'mongodb://127.0.0.1:27017/social_network';
-
-const connectDB = async () => {
+const connectDB = async (dbURI = process.env.MONGO_URI) => {
   try {
     await mongoose.connect(dbURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('✅ Conexión a MongoDB/social_network exitosa');
+    console.log("✅ Conexión a MongoDB exitosa");
   } catch (error) {
-    console.error('❌ Error al conectar a MongoDB:', error);
-    process.exit(1); // Salir del proceso si falla la conexión
+    console.error("❌ Error al conectar a MongoDB:", error);
+    process.exit(1);
   }
 };
 
